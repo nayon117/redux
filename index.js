46125a2d2f93b07d0f24898881b1657da3241c25
nayon117 <hasibul.nayon1@gmail.com>
2024-01-16 12:27:32 +0600

    setup redux and learn about store and reducer

---
+++ index.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+// store
+const store = createStore(reducer);
+
+const history = [];
+
+// reducer
+function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    return { amount: state.amount + 1 };
+  }
+  return state;
+}
+
+// global state
+
+store.subscribe(() => {
+    history.push(store.getState())
+  console.log(history);
+});
+
+setInterval(() => {
+  store.dispatch({ type: "increment" });
+},2000);
